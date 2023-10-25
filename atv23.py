6de0d16ca99e9920782633310260e7bd2e4aea1e
sofymiranda <mirandaspfya@gmail.com>
2023-10-25 11:46:47 -0300

    atividade 23

---
+++ atv23.py
@@ -0,0 +1,7 @@
+# Exercício Python 23: Faça um programa que calcule a soma entre todos os números que são múltiplos de três
+# e que se encontram no intervalo de 1 até 500.
+list = {1,500}
+for list in range (1,500):
+    if list % 3 == 0:
+     print (list)
+        
