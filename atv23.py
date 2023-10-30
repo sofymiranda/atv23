e017a11578f6b113178144976bbd64d02c15cba6
Sofya <147430717+sofymiranda@users.noreply.github.com>
2023-10-30 13:45:56 -0300

    Update atv23.py

--- atv23.py
+++ atv23.py
@@ -1,7 +1,8 @@
 # Exercício Python 23: Faça um programa que calcule a soma entre todos os números que são múltiplos de três
 # e que se encontram no intervalo de 1 até 500.
-list = {1,500}
-for list in range (1,500):
-    if list % 3 == 0:
-     print (list)
-        
+soma = 0
+for i in range(1,500):
+     if i % 3 == 0:
+        print(i)
+        soma += i
+        print (f"A soma é: {soma}")
